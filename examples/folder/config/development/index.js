7ad7abb7c397ef5b91a13a8fc24ec005d191f42f
Ariel Flesler <aflesler@gmail.com>
2014-03-26 21:57:31 -0300

    folder example uses JSON, seems like a better example

--- examples/folder/config/development/index.js
+++ examples/folder/config/development/index.js
@@ -1,3 +1,4 @@
 module.exports = {
-	messages: require('./messages')
+	messages: require('./messages'),
+	db: require('./db')
 };
--- examples/folder/config/development/messages.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-	location: "We are in development"
-};
